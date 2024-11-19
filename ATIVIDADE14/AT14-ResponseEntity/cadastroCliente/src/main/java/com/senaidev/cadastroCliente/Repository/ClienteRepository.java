13a5f9d9891e8146263eb470323fe1f7570d101d
TuannyThomazelli <159180638+TuannyThomazelli@users.noreply.github.com>
2024-11-19 10:58:57 -0300

    Delete ATIVIDADE14 directory

--- ATIVIDADE14/AT14-ResponseEntity/cadastroCliente/src/main/java/com/senaidev/cadastroCliente/Repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senaidev.cadastroCliente.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senaidev.cadastroCliente.Entities.Cliente;
-
-public interface ClienteRepository  extends JpaRepository<Cliente,Long>{
-
-}
--- ATIVIDADE14/AT14-ResponseEntity/cadastroCliente/src/main/java/com/senaidev/cadastroCliente/Repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senaidev.cadastroCliente.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senaidev.cadastroCliente.Entities.Endereco;
-
-public interface EnderecoRepository  extends JpaRepository<Endereco, Long>{
-
-}
